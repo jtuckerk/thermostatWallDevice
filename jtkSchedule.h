2ef9d05d9af472a56dfe4ac6dc45df9ac04f4f12
James Kirven <james.t.kirven@vanderbilt.edu>
2015-03-08 14:41:41 -0500

    add schedule

---
+++ jtkSchedule.h
@@ -0,0 +1,28 @@
+#ifndef jtkSchedule_h
+#define jtkSchedule_h
+
+#include "Arduino.h"
+
+#define SLICES_PER_DAY 24*4
+#define DAYS 7
+class jtkSchedule{
+
+ public:
+
+  void update();
+
+ private:
+  void setTime();
+
+  timeSlice schedule[DAYS][SLICES_PER_DAY];
+  
+  struct schedule {
+    
+  }
+  
+  struct timeSlice {
+    int temp;
+    
+  }
+
+#endif
