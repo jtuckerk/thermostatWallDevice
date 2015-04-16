04e4e2bcaf9a2f48be1a8c6fa7a9c2a38de70e3e
James Kirven <james.t.kirven@vanderbilt.edu>
2015-04-15 23:05:04 -0500

    remove

--- ThermostatIOS/LoginViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LoginViewController.swift
-//  SimpleThermostat
-//
-//  Created by Tucker Kirven on 3/25/15.
-//  Copyright (c) 2015 Tucker Kirven. All rights reserved.
-//
-
-import Foundation
--- ThermostatIOS/WirelessConnection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WirelessConnection.swift
-//  SimpleThermostat
-//
-//  Created by Tucker Kirven on 3/25/15.
-//  Copyright (c) 2015 Tucker Kirven. All rights reserved.
-//
-
-import Foundation
